19770ae6fbeed13bd648f30eeab089eeb3f3d3d6
MatteoPellegrinii <pellegrini.matteo00@gmail.com>
2022-05-05 15:38:53 +0200

    lista

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
 const ul = document.querySelector("ul.list");
+const li = document.createElement ("li");
 
-for (let index=1; index <= 100; index++){
-    const ul = `<li class=`;
+for (let index = 1; index <= 100; index++) {
+    let element = `<li class="box box--${index}">${index}</li>`;
+    ul.innerHTML += element; 
 }
-
-
-
-
-for (let i = 1; i <= 100; i++) {
-console.log(i); 
-}
-
