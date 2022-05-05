1bfc9927a11f30b07bd4eb382ecf4e81e65e8ed6
MatteoPellegrinii <pellegrini.matteo00@gmail.com>
2022-05-05 15:28:11 +0200

    inizio

---
+++ script.js
@@ -0,0 +1,13 @@
+const ul = document.querySelector("ul.list");
+
+for (let index=1; index <= 100; index++){
+    const ul = `<li class=`;
+}
+
+
+
+
+for (let i = 1; i <= 100; i++) {
+console.log(i); 
+}
+
