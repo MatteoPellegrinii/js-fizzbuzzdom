d3b274768ff19ee90ddd402f565fe3f267b11d28
MatteoPellegrinii <pellegrini.matteo00@gmail.com>
2022-05-05 16:39:27 +0200

    fine

--- script.js
+++ script.js
@@ -2,25 +2,25 @@ const ul = document.querySelector("ul.list");
 const li = document.createElement ("li");
 
 for (let index = 1; index <= 100; index++) {
-    let restodicinque = index % 3;
-    console.log(restodicinque);
-    let restodidieci = index % 5;
-    console.log(restodidieci);
+    let reestotre = index % 3;
+    console.log(reestotre);
+    let restocinque = index % 5;
+    console.log(restocinque);
     
-    if (restodicinque === 0){
-        let element = `<li class="box box--${index}"> fizz </li>`;
-        ul.innerHTML += element;
-    }
-    else if (restodidieci=== 0) {
-        let element = `<li class="box box--${index}"> buzz </li>`;
-        ul.innerHTML += element;
-    }
-    else if((restodicinque === 0) || (restodidieci=== 0)) {
+    if( reestotre==0 && restocinque==0) {
         let element = `<li class="box box--${index}"> fizzbuzz </li>`;
         ul.innerHTML += element;
     }
+    else if (restocinque=== 0) {
+        let element = `<li class="cinque cinque--${index}"> buzz </li>`;
+        ul.innerHTML += element;
+    }
+    else if (reestotre === 0){
+        let element = `<li class="fizz fizz--${index}"> fizz </li>`;
+        ul.innerHTML += element;
+    }
     else{
-        let element = `<li class="box box--${index}">${index}</li>`;
+        let element = `<li class="others others--${index}">${index}</li>`;
         ul.innerHTML += element; 
     }
 }
