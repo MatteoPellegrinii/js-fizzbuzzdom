10ba8830c094b0847ae04fc5ea148596fb0a33c1
MatteoPellegrinii <pellegrini.matteo00@gmail.com>
2022-05-05 15:59:11 +0200

    rsto

--- script.js
+++ script.js
@@ -4,4 +4,15 @@ const li = document.createElement ("li");
 for (let index = 1; index <= 100; index++) {
     let element = `<li class="box box--${index}">${index}</li>`;
     ul.innerHTML += element; 
+    let restodicinque = index % 5;
+    console.log(restodicinque);
+    let restodidieci = index % 10;
+    console.log(restodidieci);
+    
+    if (resto = 0){
+        let element = `fizzbuz`;
+        ul.innerHTML += element; 
+    }
 }
+
+
